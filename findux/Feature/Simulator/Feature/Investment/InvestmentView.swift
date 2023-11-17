7290d5ac3d6a93810c27fa869bc8a9784fa009b1
periclesnarbal <periclesnarbal@gmail.com>
2023-11-17 16:00:01 -0300

    tratamento de campos da tela de investimentos

--- findux/Feature/Simulator/Feature/Investment/InvestmentView.swift
+++ findux/Feature/Simulator/Feature/Investment/InvestmentView.swift
@@ -9,6 +9,34 @@ import UIKit
 import SwiftUI
 
 class InvestmentView: BaseView<SimulatorCoordinator> {
+    enum MonthYear: String {
+        case month = "Meses"
+        case year = "Anos"
+        case invalid = ""
+        
+        init?(rawValue: String) {
+            switch rawValue {
+            case "Meses": self = .month
+            case "Anos": self = .year
+            default: self = .invalid
+            }
+        }
+    }
+    
+    @IBOutlet weak var investingTypeSegment: UISegmentedControl!
+    @IBOutlet weak var fixingTypeSegment: UISegmentedControl!
+    @IBOutlet weak var termUnitSegment: UISegmentedControl!
+    @IBOutlet weak var profitUnitSegment: UISegmentedControl!
+    
+    @IBOutlet weak var startInvestingAmountField: UITextField!
+    @IBOutlet weak var monthlyInvestingField: UITextField!
+    @IBOutlet weak var investingTermField: UITextField!
+    @IBOutlet weak var profitRateField: UITextField!
+    @IBOutlet weak var finalValueField: UITextField!
+    
+    @IBOutlet var fieldArray: [UITextField]!
+    
+    @IBOutlet weak var continueButton: UIButton!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,6 +52,69 @@ class InvestmentView: BaseView<SimulatorCoordinator> {
         if let view: UIView = loadViewFromNib() {
             addViewToBounds(view)
         }
+        
+        startInvestingAmountField.addTarget(self, action: #selector(formatCurrencyTextInput(sender: )), for: .editingChanged)
+        monthlyInvestingField.addTarget(self, action: #selector(formatCurrencyTextInput(sender: )), for: .editingChanged)
+        finalValueField.addTarget(self, action: #selector(formatCurrencyTextInput(sender: )), for: .editingChanged)
+        
+        termUnitSegment.addTarget(self, action: #selector(formatTermUnit(sender: )), for: .valueChanged)
+        profitUnitSegment.addTarget(self, action: #selector(formatProfitUnit(sender: )), for: .valueChanged)
+    }
+    
+    @objc func formatCurrencyTextInput(sender: UITextField) {
+        let formatedEntry = sender.text?.fromDecimalCurrency()
+        sender.text = formatedEntry?.toDecimalCurrency()
+    }
+    
+    @objc func formatTermUnit(sender: UISegmentedControl) {
+        var timeValue: Double = 0
+        
+        guard let field = investingTermField,
+              let unit = getTimeUnit(from: sender),
+              let inputValue = field.text?.toDouble() else { return }
+        
+        switch unit {
+        case .month: timeValue = yearsToMonths(from: inputValue)
+        case .year: timeValue = monthsToYears(from: inputValue)
+        case .invalid: return
+        }
+        
+        let isInteger = timeValue.truncatingRemainder(dividingBy: 1) == 0
+        let formatedResult = isInteger ? String(format: "%.0f", timeValue) : String(format: "%.2f", timeValue)
+        
+        field.text = formatedResult.replacingDotsForCommas()
+    }
+    
+    @objc func formatProfitUnit(sender: UISegmentedControl) {
+        var timeValue: Double = 0
+        
+        guard let field = profitRateField,
+              let unit = getTimeUnit(from: sender),
+              let inputValue = field.text?.toDouble() else { return }
+        
+        switch unit {
+        case .month: timeValue = monthsToYears(from: inputValue)
+        case .year: timeValue = yearsToMonths(from: inputValue)
+        case .invalid: return
+        }
+        
+        field.text = String(timeValue).replacingDotsForCommas()
+    }
+    
+    func getTimeUnit(from segment: UISegmentedControl) -> MonthYear? {
+        let selectedIndex = segment.selectedSegmentIndex
+        if let title = segment.titleForSegment(at: selectedIndex) {
+            return MonthYear(rawValue: title)
+        }
+       return nil
+    }
+
+    func yearsToMonths(from value: Double) -> Double {
+        return value * 12
+    }
+
+    func monthsToYears(from value: Double) -> Double  {
+        return value / 12
     }
 }
 
---
+++ findux/General/Extension/StringExtension.swift
@@ -0,0 +1,52 @@
+//
+//  StringExtension.swift
+//  findux
+//
+//  Created by Péricles Narbal on 16/11/23.
+//
+
+import Foundation
+
+extension String {
+    func toDecimalCurrency() -> String {
+        guard let number = Int(self) else { return self }
+        let decimal = Double(number) / 100.0
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.decimalSeparator = ","
+        formatter.locale = Locale(identifier: "pt_BR")
+        
+        if let formattedString = formatter.string(from: NSNumber(value: decimal)) {
+            return formattedString
+        }
+        
+        return self
+    }
+    
+    func fromDecimalCurrency() -> String {
+        return removeCommas(from: self)
+    }
+    
+    func removeCommas(from text: String) -> String {
+        let stringWithoutCommas = text.replacingOccurrences(of: ",", with: "")
+        return stringWithoutCommas
+    }
+    
+    func replacingDotsForCommas() -> String {
+        return self.replacingOccurrences(of: ".", with: ",")
+    }
+    
+    func toDouble() -> Double? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.decimalSeparator = ","
+        formatter.locale = Locale(identifier: "pt_BR")
+        
+        if let number = formatter.number(from: self.replacingDotsForCommas()) {
+            return number.doubleValue
+        }
+        
+        return nil
+    }
+}
